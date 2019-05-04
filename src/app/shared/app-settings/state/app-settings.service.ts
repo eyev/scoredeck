3b42e29c214925a793c9cb6339c360e25d556fb6
Justin Conklin <justin@wiredviews.com>
2019-05-04 12:38:44 -0600

    chore(): import cleanup

--- src/app/shared/app-settings/state/app-settings.service.ts
+++ src/app/shared/app-settings/state/app-settings.service.ts
@@ -1,14 +1,10 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 import { AppSettings, AppSettingsStore } from './app-settings.store';
 
 @Injectable({ providedIn: 'root' })
 export class AppSettingsService {
-  constructor(
-    private appSettingsStore: AppSettingsStore,
-    private http: HttpClient,
-  ) {}
+  constructor(private appSettingsStore: AppSettingsStore) {}
   set(settings: Partial<AppSettings>) {
     this.appSettingsStore.update(state => {
       window.localStorage.setItem(
