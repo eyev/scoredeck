25a9f08cbe2e60cfe5fca28e118ee5c17833aca8
Justin Conklin <xtacide@gmail.com>
2019-05-06 02:22:49 -0400

    fix(appSettingsService): set overlayContainer class when switching themes

--- src/app/shared/app-settings/state/app-settings.service.ts
+++ src/app/shared/app-settings/state/app-settings.service.ts
@@ -1,11 +1,18 @@
+import { OverlayContainer } from '@angular/cdk/overlay';
 import { Injectable } from '@angular/core';
 
 import { AppSettings, AppSettingsStore } from './app-settings.store';
 
 @Injectable({ providedIn: 'root' })
 export class AppSettingsService {
-  constructor(private appSettingsStore: AppSettingsStore) {}
+  constructor(
+    private appSettingsStore: AppSettingsStore,
+    private overlayContainer: OverlayContainer,
+  ) {}
   set(settings: Partial<AppSettings>) {
+    if (settings.theme === 'light') {
+      this.overlayContainer.getContainerElement().classList.add('day-theme');
+    }
     this.appSettingsStore.update(state => {
       window.localStorage.setItem(
         'appSettings',
